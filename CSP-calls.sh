94a64cba7958a375f628677b4eccf2833c3358ec
BeaVillacorta <100451062@alumnos.uc3m.es>
2022-12-13 15:36:48 +0100

    añadiendo ficheros de prueba y CSP calls

---
+++ CSP-calls.sh
@@ -0,0 +1,7 @@
+echo "huele a estufa"
+
+./python CSPCargaBUS.py ./CSP-tests/alumnos.txt
+./python CSPCargaBUS.py ./CSP-tests/alumnos2.txt
+./python CSPCargaBUS.py ./CSP-tests/alumnos3.txt
+./python CSPCargaBUS.py ./CSP-tests/alumnos4.txt
+./python CSPCargaBUS.py ./CSP-tests/alumnos3noSolucion.txt
