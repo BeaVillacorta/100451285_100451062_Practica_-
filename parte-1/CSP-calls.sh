4e772589e4208ff1f14bd748ea14f60ec30da623
BeaVillacorta <100451062@alumnos.uc3m.es>
2022-12-15 16:18:43 +0100

    Mejoras estructura

--- parte-1/CSP-calls.sh
+++ parte-1/CSP-calls.sh
@@ -1,7 +1,8 @@
 echo "parte1"
 
-./python CSPCargaBUS.py ./CSP-tests/alumnos.txt
-./python CSPCargaBUS.py ./CSP-tests/alumnos2.txt
-./python CSPCargaBUS.py ./CSP-tests/alumnos3.txt
-./python CSPCargaBUS.py ./CSP-tests/alumnos4.txt
-./python CSPCargaBUS.py ./CSP-tests/alumnos3noSolucion.txt
+./python parte-1/CSPCargaBUS.py ./parte-1/CSP-tests/alumnos.txt
+./python parte-1/CSPCargaBUS.py ./parte-1/CSP-tests/alumnos2.txt
+./python parte-1/CSPCargaBUS.py ./parte-1/CSP-tests/alumnos3.txt
+./python parte-1/CSPCargaBUS.py ./parte-1/CSP-tests/alumnos4.txt
+./python parte-1/CSPCargaBUS.py ./parte-1/CSP-tests/alumnos3noSolucion.txt
+./python parte-1/CSPCargaBUS.py ./parte-1/CSP-tests/alumnos5.txt
---
+++ parte-2/ASTAR-calls.sh
@@ -0,0 +1,3 @@
+echo "parte2"
+
+./python parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos.prob
