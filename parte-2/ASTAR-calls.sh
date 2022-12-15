6956ad4e1de48eb75fc045975d3f4c2e5f81bf9a
BeaVillacorta <100451062@alumnos.uc3m.es>
2022-12-15 22:13:37 +0100

    mas casos de prueba

--- parte-2/ASTAR-calls.sh
+++ parte-2/ASTAR-calls.sh
@@ -1,3 +1,10 @@
 echo "parte2"
 
-python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos.prob
+
+python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos.prob 1
+python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos2.prob 1
+python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos3.prob 1
+python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos.prob 2
+python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos2.prob 2
+python ./parte-2/ASTARColaBus.py ./parte-2/ASTAR-tests/alumnos3.prob 2
+
